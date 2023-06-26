6110f0b2cb4142a0dad61cae0baceb307924b28f
Andrew Hart <a.p.hart97@gmail.com>
2023-06-26 11:39:34 -0400

    Add save_dir

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -2,20 +2,20 @@ from PyQt5.QtWidgets import QDialog, QTabWidget, QVBoxLayout
 from src.peripherals.Webcam import CameraTab
 
 class ClassifyTab(CameraTab):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, model_dir):
+        super().__init__(model_dir)
 
         # Set the tab name
         self.tab_name = "Image Prediction"
 
 
 class MainWindow(QDialog):
-    def __init__(self):
+    def __init__(self, model_dir):
         super().__init__()
 
         # Create the tab widget
         self.tab_widget = QTabWidget()
-        self.tab_widget.addTab(ClassifyTab(), "Classify")
+        self.tab_widget.addTab(ClassifyTab(model_dir), "Classify")
 
         # Create the layout and add the tab widget
         layout = QVBoxLayout()
